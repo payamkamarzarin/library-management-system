a7081a0b827e39dbe42cfb7d58ceb67a0e6b9e26
Payam Kamarzarin <payamkamarzarin@yahoo.com>
2024-11-30 16:24:08 +0330

    Creating a connection file to the database

--- config/connection.php
+++ config/connection.php
@@ -1,1 +1,24 @@
 <?php
+class DatabaseConnection
+{
+    private $host = "localhost";
+    private $database = "library";
+    private $username = "root";
+    private $password = "";
+    private $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=$this->host;port=3308;dbname=$this->database", $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection Faild: " . $e->getMessage();
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->pdo;
+    }
+}
